555aa6baf455f116525a8203363bdf31f6f3b21b
Gema Ramadhan <gema.ramadhan.ft21@mail.umy.ac.id>
2023-12-28 15:01:26 +0700

    Menambah Composable FormInputSiswa() dengan fungsi input terstruktur untuk nama, alamat email, dan nomor telepon dengan pemeriksaan data yang diperlukan

--- app/src/main/java/com/example/kontak/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/kontak/ui/PenyediaViewModel.kt
@@ -4,19 +4,19 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.kontak.KontakApplication
+import com.example.kontak.KontakAplication
 import com.example.kontak.ui.home.viewmodel.HomeViewModel
 import com.example.kontak.ui.home.viewmodel.InsertViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
-            HomeViewModel(aplikasiMars().container.kontakRepository)
+            HomeViewModel(aplikasiKontak().container.kontakRepository)
         }
         initializer {
-            InsertViewModel(aplikasiMars().container.kontakRepository)
+            InsertViewModel(aplikasiKontak().container.kontakRepository)
         }
     }
 }
-fun CreationExtras.aplikasiMars(): KontakApplication =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as KontakApplication)
+fun CreationExtras.aplikasiKontak(): KontakAplication =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as KontakAplication)
