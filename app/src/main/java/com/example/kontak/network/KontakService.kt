9ae443b5ef3279b9e3c3f60140c75c85ce2fafba
Gema Ramadhan <gema.ramadhan.ft21@mail.umy.ac.id>
2023-12-28 13:45:06 +0700

    Menambah fungsi deleteKontak() dengan metode DELETE pada endpoint "kontak/{id}" untuk menghapus kontak berdasarkan ID

--- app/src/main/java/com/example/kontak/network/KontakService.kt
+++ app/src/main/java/com/example/kontak/network/KontakService.kt
@@ -1,7 +1,9 @@
 package com.example.kontak.network
 
 import com.example.kontak.model.Kontak
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
@@ -17,12 +19,17 @@ interface KontakService {
 
     @GET("kontak/{id}")
     suspend fun getKontakById(@Path("id") id: Int): Kontak
+
+
+    @POST("kontak")
+    suspend fun insertKontak(@Body kontak: Kontak)
+
+
+    @PUT("kontak/{id}")
+    suspend fun updateKontak(@Path("id") id: Int, @Body kontak: Kontak)
+
+
+    @DELETE("kontak/{id}")
+    suspend fun deleteKontak(@Path("id") id: Int): Response<Void>
 }
 
-@POST("kontak")
-suspend fun insertKontak(@Body kontak: Kontak) {
-}
-
-@PUT("kontak/{id}")
-suspend fun updateKontak(@Path("id") id: Int, @Body kontak: Kontak) {
-}
