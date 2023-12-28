688807550ed1b3da5f12e78ba549652cc08e4fc7
Gema Ramadhan <gema.ramadhan.ft21@mail.umy.ac.id>
2023-12-28 14:11:56 +0700

    Menambah fungsi insertKontak() dengan penggunaan viewModelScope untuk menyimpan kontak ke repository

--- app/src/main/java/com/example/kontak/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/kontak/ui/home/viewmodel/InsertViewModel.kt
@@ -4,7 +4,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.kontak.repository.KontakRepository
+import kotlinx.coroutines.launch
 
 class InsertViewModel (private val kontakRepository: KontakRepository) : ViewModel(){
     var insertKontakState by mutableStateOf(InsertUiState())
@@ -13,3 +15,13 @@ class InsertViewModel (private val kontakRepository: KontakRepository) : ViewMod
     fun updateInsertKontakState(insertUiEvent: InsertUiEvent){
         insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
     }
+    suspend fun insertKontak(){
+        viewModelScope.launch {
+            try {
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
