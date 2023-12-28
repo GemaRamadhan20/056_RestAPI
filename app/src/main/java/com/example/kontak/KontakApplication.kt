a1966689065c425134267091b15512da0198d197
Gema Ramadhan <gema.ramadhan.ft21@mail.umy.ac.id>
2023-12-28 14:57:14 +0700

    Tambah kelas KontakApplication dengan inisialisasi container pada onCreate()

--- app/src/main/java/com/example/kontak/KontakApplication.kt
+++ app/src/main/java/com/example/kontak/KontakApplication.kt
@@ -4,10 +4,8 @@ import android.app.Application
 import com.example.kontak.repository.AppContainer
 import com.example.kontak.repository.KontakContainer
 
-class KontakApplication : Application() {
-    /** AppContainer Instance Used By the rest of classes */
+class KontakAplication : Application() {
     lateinit var container: AppContainer
-
     override fun onCreate() {
         super.onCreate()
         container = KontakContainer()
