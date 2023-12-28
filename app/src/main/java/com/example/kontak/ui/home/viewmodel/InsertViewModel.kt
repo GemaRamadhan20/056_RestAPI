75242e30fd5e3b90e9e4c296a5fc39746e10823d
Gema Ramadhan <gema.ramadhan.ft21@mail.umy.ac.id>
2023-12-28 14:13:18 +0700

    Menambah data class InsertUiEvent sebagai representasi event UI untuk operasi penyisipan data

--- app/src/main/java/com/example/kontak/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/kontak/ui/home/viewmodel/InsertViewModel.kt
@@ -25,3 +25,10 @@ class InsertViewModel (private val kontakRepository: KontakRepository) : ViewMod
         }
     }
 }
+data class InsertUiEvent(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val nohp: String = "",
+)
+
